f28133de0ec7813605b68fc3232c40d5a3fc517f
SibhatY <sabto6659@gmail.com>
2024-10-03 21:42:24 -0500

    Addition of habit deletion feature, closes #2

--- client/src/components/HabitList.js
+++ client/src/components/HabitList.js
@@ -1,6 +1,12 @@
 import React from "react";
 
-const HabitList = ({habits}) => {
+const HabitList = ({habits, setHabits}) => {
+
+
+    const deleteHandling = (id) => {
+
+        setHabits((prevHabits) => prevHabits.filter((habit) => habit.id !== id));
+    };
 
     return (
         <div>
@@ -9,6 +15,7 @@ const HabitList = ({habits}) => {
                 <div key={habit.id}>
                     <h3>{habit.title}</h3>
                     <p>Progress: {habit.progress}%</p>
+                    <button onClick={() => deleteHandling(habit.id)}>Delete</button>
                 </div>
             ))}
         </div>
